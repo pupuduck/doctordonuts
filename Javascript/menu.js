4899f625f29771b82ccf946dce1300d9955bfae1
pupuduck <ernestwongdexing@gmail.com>
2022-07-18 14:08:52 +0800

    sacve

--- Javascript/menu.js
+++ Javascript/menu.js
@@ -1,26 +1,28 @@
 let menuOne = document.getElementById("menu1")
 let menuTwo = document.getElementById("menu2")
 let menuThree = document.getElementById("menu3")
-function m1(){
+
+
+menuOne.addEventListener("click", () => {
     document.getElementById("display").innerHTML = document.getElementById("m1").innerHTML
     menuOne.classList.remove("removebackground")
     menuOne.classList.add("addbackground")
     menuTwo.classList.add("removebackground")
     menuThree.classList.add("removebackground")
-}
-function m2(){
+})
+menuTwo.addEventListener("click", () => {
     document.getElementById("display").innerHTML = document.getElementById("m2").innerHTML
     menuOne.classList.add("removebackground")
     menuTwo.classList.remove("removebackground")
     menuTwo.classList.add("addbackground")
     menuThree.classList.add("removebackground")
-}
-function m3(){
+})
+menuThree.addEventListener("click", () => {
     document.getElementById("display").innerHTML = document.getElementById("m3").innerHTML
     menuOne.classList.add("removebackground")
     menuTwo.classList.add("removebackground")
     menuThree.classList.add("addbackground")
     menuThree.classList.remove("removebackground")
-}
+})
 
 
