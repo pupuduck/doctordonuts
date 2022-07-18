3090600eb18f24cd34ce907d417562d91138655f
pupuduck <ernestwongdexing@gmail.com>
2022-07-18 20:47:44 +0800

    update

--- Javascript/contact.js
+++ Javascript/contact.js
@@ -6,16 +6,15 @@ let heading = document.getElementById("headingall");
 const form = document.getElementById("form");
 
 // adding event listener //
-form.addEventListener("submit", openPopup)
+form.addEventListener("submit", () => {
+    popup.classList.add("open-popup");
+    field.classList.add("remove-field");
+    field2.classList.add("remove-field");
+    heading.classList.add("remove-field");
+    document.getElementById("name-input").innerHTML = document.getElementById("name").value;
+})
 
-//removes the form and details, adds pop up class//
-function openPopup(){
-        popup.classList.add("open-popup");
-        field.classList.add("remove-field");
-        field2.classList.add("remove-field");
-        heading.classList.add("remove-field");
-        document.getElementById("name-input").innerHTML = document.getElementById("name").value;
-}
+
 //* closes the pop up //
 function closePopup(){
     popup.classList.remove("open-popup")
